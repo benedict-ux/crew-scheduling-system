0ce879d762a72d1d02f88d6e68796f33ed7ee08c
benedict-ux <benedictcarungcong@gmail.com>
2026-03-23 11:23:02 +0800

    fixing the login

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Jollibee Crew Scheduling System
 // Caches static files for faster mobile loading
 
-const CACHE_NAME = 'jollibee-crew-v22.4';
+const CACHE_NAME = 'jollibee-crew-v22.7';
 const urlsToCache = [
   '/',
   '/login.html',
